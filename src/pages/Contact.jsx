6ac30cb524f42364ebc51b2f14d7d6df14e4fddf
vicky kumar <vickyk5244@gmail.com>
2026-01-10 12:38:48 +0530

    some function add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,105 +1,157 @@
 import { useState } from "react";
+
 export default function Contact() {
 
-    const defaultContactFormData = {
-        name: "",
-        email: "",
-        number: "",
-        message: ""
+  const defaultContactFormData = {
+    name: "",
+    email: "",
+    number: "",
+    message: ""
+  };
+
+  const [contact, setContact] = useState(defaultContactFormData);
+  const [errors, setErrors] = useState({});
+
+  function inputhandling(event) {
+    let fieldName = event.target.name;
+    let valueName = event.target.value;
+
+    setContact((currValue) => ({
+      ...currValue,
+      [fieldName]: valueName,
+    }));
+  }
+
+  //  Validation function
+  const validateForm = () => {
+    let newErrors = {};
+
+    // Email validation
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(contact.email)) {
+      newErrors.email = "Please enter a valid email address";
     }
 
-   let [contact, setContact] = useState(defaultContactFormData);
+    // Mobile number validation (India)
+    if (!/^[6-9]\d{9}$/.test(contact.number)) {
+      newErrors.number = "Please enter a valid 10-digit mobile number";
+    }
 
-    function inputhandling(event) {
-        let fieldName = event.target.name;
-        let valueName = event.target.value;
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
 
-        setContact((currValue) => ({
-         ...currValue,
-         [fieldName]: valueName,
-     }))};
+  const submithandling = async (event) => {
+    event.preventDefault();
 
+    // Agar invalid hai to submit hi nahi hoga
+    if (!validateForm()) return;
 
-    const submithandling = async(event) => {
-        event.preventDefault();
-        
-        try {
-            
-            const response = await fetch('https://react-portfolio-oicx.onrender.com/api/contact', { 
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json',
-            },
-            body: JSON.stringify(contact),
+    try {
+      const response = await fetch(
+        "https://react-portfolio-oicx.onrender.com/api/contact",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(contact),
+        }
+      );
 
-           });
-
-           if(response.ok) {
-               setContact(defaultContactFormData);
-               alert("message sent successfully");
-           }
-
-        } catch (error) {
-            console.log("contact", error);
-        }   
+      if (response.ok) {
+        setContact(defaultContactFormData);
+        setErrors({});
+        alert("Message sent successfully ✅");
       }
+    } catch (error) {
+      console.log("contact", error);
+    }
+  };
 
-    return (
-       <main>
-              <section>
-                 <div className="home-container">
-                    <div className="home-details">
-                        
-                    <form onSubmit={submithandling}>
-                            <label htmlFor="username"><h2><i><b>Name</b></i></h2></label>
-                            <br></br>
-                            
-                            <input value={contact.name} id="username" onChange={inputhandling} name="name" required/> 
-                            <br></br>
-                            <br></br>
-                             <label htmlFor="email"><h2><i><b>Email Address</b></i></h2></label>
-                             <br></br>
-                            
-                            <input value={contact.email} id="email" onChange={inputhandling} name="email" required/>
-                            <br></br>
-                            <br></br>
-                             <label htmlFor="number"><h2><i><b>Mobile Number</b></i></h2></label>
-                             <br></br>
-                            
-                            <input value={contact.number} id="number" onChange={inputhandling} name="number" required/>
-                            <br></br>
-                            <br></br>
-                             <label htmlFor="message"><h2><i><b>Message</b></i></h2></label>
-                             <br></br>
-                            
-                            <textarea value={contact.message} id="message" onChange={inputhandling} name="message" required/>
-                            <br></br>
-                            <br></br>
-                            <button type="submit" className="submit-butt">Submit</button>
-                            
-                            
-                        </form>
-                    
-                    </div>
+  return (
+    <main>
+      <section>
+        <div className="home-container">
+          <div className="home-details">
 
-                    <div>
-                    <div className="contant-picture">
+            <form onSubmit={submithandling}>
 
-                        
-                        
+              <label htmlFor="username">
+                <h2><i><b>Name</b></i></h2>
+              </label>
+              <input
+                value={contact.name}
+                id="username"
+                onChange={inputhandling}
+                name="name"
+                required
+              />
 
-                    </div>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <div className="contant-me">
-                       <h4><b><i>Whatsapp-Number:- 9905649234</i></b></h4>
-                       <br></br>
-                       <h4><b><i>Email-Id:- vickyk5244@gmail.com</i></b></h4>
-                    </div>
-                    </div>
-                 </div>
-              </section>
-       </main>
-    )
+              <br /><br />
+
+              <label htmlFor="email">
+                <h2><i><b>Email Address</b></i></h2>
+              </label>
+              <input
+                value={contact.email}
+                id="email"
+                onChange={inputhandling}
+                name="email"
+                required
+              />
+              {errors.email && <p className="error">{errors.email}</p>}
+
+              <br /><br />
+
+
+              <label htmlFor="number">
+                <h2><i><b>Mobile Number</b></i></h2>
+              </label>
+              <input
+                value={contact.number}
+                id="number"
+                onChange={inputhandling}
+                name="number"
+                required
+              />
+              {errors.number && <p className="error">{errors.number}</p>}
+
+              <br /><br />
+
+              <label htmlFor="message">
+                <h2><i><b>Message</b></i></h2>
+              </label>
+              <textarea
+                value={contact.message}
+                id="message"
+                onChange={inputhandling}
+                name="message"
+                required
+              />
+
+              <br /><br />
+
+              <button type="submit" className="submit-butt">
+                Submit
+              </button>
+
+            </form>
+          </div>
+
+          <div>
+            <div className="contant-picture"></div>
+
+            <br /><br /><br />
+
+            <div className="contant-me">
+              <h4><b><i>Whatsapp-Number:- 9905649234</i></b></h4>
+              <br />
+              <h4><b><i>Email-Id:- vickyk5244@gmail.com</i></b></h4>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </main>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,53 +1,117 @@
 
+import { useState , useEffect} from "react";
+
 export default function Home() {
-    return (
-       <main>
-           <section className="home-section">
-                <div className="home-main-container">
-                    <div className="home-main-details">
-                        <h3>Hi, I'm <i id="name">Vicky Kumar</i></h3>
-                        <br></br>
-                        <h2>MERN Stack Developer</h2>
-                        <br></br>
-                        <br></br>
-                        <h4><i>I build responsive, scalable and user-friendly Web Applications 
-                            using React, Node.js, Express and MongoDB.</i></h4>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <div className="button-container">
-                            <div>
-                                <a href="/projects">
-                                    <button id="view-button">View Projects</button>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="/contact">
-                                    <button id="view-button1">Contact</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="picture-main">
-                          
-                    </div>
-                </div>
-           </section>
+
+  // Qualification Data
+  const qualifications = [
+    {
+      title: "10th Bihar Board (2021)",
+      college: "Uttkramit Madhyamik School, Rohtas, bihar",
+      marks: "83%"
+    },
+    {
+      title: "12th Bihar Bord (2023)",
+      college: "S.B College, Ara",
+      marks: "80% (PCM)"
+    },
+    {
+      title: "BE in Computer science of engineering (2023-2027)",
+      college: "Sengunthar Engineering College, Tamilnadu",
+      marks: "8.1 CGPA (Currently pursing 3rd year)"
+    },
+    {
+      title: "MERN Stack",
+      college: "Self Learning",
+      marks: "React, Node, Express, MongoDB"
+    }
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+  const timer = setInterval(() => {
+    setIndex((prevIndex) =>
+      prevIndex === qualifications.length - 1 ? 0 : prevIndex + 1
+    );
+  }, 3000); // 3 second me slide change
+
+  return () => clearInterval(timer);
+}, []);
+
+
+  const prev = () => {
+    setIndex(index === 0 ? qualifications.length - 1 : index - 1);
+  };
+
+  const next = () => {
+    setIndex(index === qualifications.length - 1 ? 0 : index + 1);
+  };
+
+  return (
+    <main>
+      
+      <section className="home-section">
+        <div className="home-main-container">
+          <div className="home-main-details">
+            <h3>Hi, I'm <i id="name">Vicky Kumar</i></h3>
+            <br />
+            <h2>MERN Stack Developer</h2>
+            <br /><br />
+            <h4>
+              <i>
+                I build responsive, scalable and user-friendly Web Applications
+                using React, Node.js, Express and MongoDB.
+              </i>
+            </h4>
+            <br /><br /><br />
+
+            <div className="button-container">
+              <div>
+                <a href="/projects">
+                  <button id="view-button">View Projects</button>
+                </a>
+              </div>
+              <div>
+                <a href="/contact">
+                  <button id="view-button1">Contact</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="picture-main"></div>
+        </div>
+      </section>
+
+    
+      <section className="qualification-section">
+        <h2 className="qualification-title">My Qualifications</h2>
+
+        <div className="qualification-container">
+          <button className="arrow left" onClick={prev}>❮</button>
+
+          <div className="card">
+            <h3>{qualifications[index].title}</h3>
+            <p>{qualifications[index].college}</p>
+            <span>{qualifications[index].marks}</span>
+          </div>
+
+          <button className="arrow right" onClick={next}>❯</button>
+        </div>
+      </section>
+    
            <br></br>
            <br></br>
            <br></br>
            <br></br>
            <br></br>
            <br></br>
-           <br></br>
-           <br></br>
-           <br></br>
-           <br></br>
-           <br></br>
-           <br></br>
+           
             <section className="home-section">
                 <div class="main-page-project"><h1><b><i>PROJECTS :-</i></b></h1></div>
                 <div className="home-main1-container">
+                    
                     <div className="picture-main1">
 
                     </div>
@@ -74,7 +138,7 @@ export default function Home() {
                         <br></br>
                         <h3>Tech Stack:- </h3>
                          <br></br>
-                        <h3>HTML, CSS, JavaScript, React.js, Node.js, Express.js, MongoDB</h3>
+                        <h3>React.js, Node.js, Express.js, MongoDB</h3>
                         <br></br>
                         
                         <h2><u>Weather App</u></h2>
